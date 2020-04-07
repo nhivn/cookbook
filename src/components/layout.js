90cb211429026842e316e3b4165f2d5220fb2d18
Nhi Nguyen <nhi.nguyen@myob.com>
2020-04-07 13:58:13 +1000

    Move font face to css file

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,14 +11,14 @@ import { useStaticQuery, graphql } from "gatsby"
 import { Grommet, grommet, Box, Footer, Anchor } from "grommet"
 import { Github } from "grommet-icons"
 
+import "./layout.css"
+
 const theme = {
   ...grommet,
   global: {
     font: {
       name: "Delius",
       family: "'Delius', Arial, sans-serif",
-      face:
-        "/* latin */\n@font-face {\n  font-family: 'Delius';\n  font-style: normal;\n  font-weight: 400;\n  src: local('Delius-Regular'), url(https://fonts.gstatic.com/s/delius/v9/PN_xRfK0pW_9e1rdZsg_rz7b_g.woff2) format('woff2');\n  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;\n}\n",
     },
   },
   checkBox: {
