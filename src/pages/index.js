075850a1379fc75d6a83af99bb4247718e14c70f
Nhi <nhivn.dev@gmail.com>
2021-07-19 22:31:23 +1000

    Remove the chef hat because I don't like it

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,20 @@
 import React from "react"
 import { graphql } from "gatsby"
 import { navigate } from "@reach/router"
-import { TextInput, Box, Anchor, Heading, ResponsiveContext } from "grommet"
+import {
+  TextInput,
+  Text,
+  Box,
+  Anchor,
+  Heading,
+  ResponsiveContext,
+} from "grommet"
 import { FormSearch } from "grommet-icons"
 import styled from "styled-components"
 
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
 import Container from "../components/Container"
-import CookbookTitle from "../components/CookbookTitle"
 
 const RecipeAnchor = styled(Anchor)`
   font-variant-caps: all-small-caps;
@@ -30,7 +36,7 @@ const IndexPage = (props) => {
     const filteredRecipes = allRecipes.filter((recipe) => {
       const { tips, note, steps, ingredients, title } = recipe.node
       return (
-        (tips && tips.toLowerCase().includes(query)) ||
+        (tips && tips.some((t) => t.toLowerCase().includes(query))) ||
         (note && note.toLowerCase().includes(query)) ||
         steps.some((s) => s.toLowerCase().includes(query)) ||
         ingredients.some((i) => i.toLowerCase().includes(query)) ||
@@ -50,12 +56,11 @@ const IndexPage = (props) => {
     <Layout animation="slideUp">
       <SEO title="Home" />
       <Container width="medium" margin="small" animation="slideUp">
-        <CookbookTitle />
-
         <Box margin={{ bottom: "medium" }}>
           <TextInput
             dropHeight="3"
-            placeholder="Search ingredient, cook style, etc..."
+            plain
+            placeholder="Search"
             icon={<FormSearch />}
             onChange={onSearchQueryChanged}
           />
